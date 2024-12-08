726918d3b9f596ec5de7de2c9fd68b8eb34400c1
aleksey3sotni <belkov.alex2005@yandex.ru>
2024-12-08 21:37:46 +0300

    Update modCipher.h

--- 3.2/modCipher.h
+++ 3.2/modCipher.h
@@ -2,13 +2,41 @@
 #include <string>
 #include <stdexcept>
 #include <vector>
+#include <algorithm>
 
-class cipher_error : public std::runtime_error {
+class cipher_error : public std::invalid_argument {
 public:
-    explicit cipher_error(const std::string& what_arg) : std::runtime_error(what_arg) {}
+    explicit cipher_error(const std::string& what_arg) : std::invalid_argument(what_arg) {}
+    explicit cipher_error(const char* what_arg) : std::invalid_argument(what_arg) {}
 };
 
-std::string prepareText(const std::string& text);
-std::string coder(int key, const std::string& text);
-std::string decoder(int key, const std::string& text);
-int getValidKey(int key, const std::string& text);
+class Code {
+private:
+    int key;
+
+    int getValidKey(int key, const std::string& text) {
+        return key; 
+    }
+
+    std::string getValidOpenText(const std::string& s) {
+        return s; 
+    }
+    
+    std::string getValidCipherText(const std::string& s, const std::string& open_text) {     
+        return s;
+    }
+    
+public:
+    Code() = delete;
+
+    Code(int skey, const std::string& text) : key(getValidKey(skey, text)) {
+    }
+
+    std::string encryption(const std::string& text) {
+        return text; 
+    }
+
+    std::string decrypt(const std::string& text, const std::string& open_text) {
+        return text; 
+    }
+};
